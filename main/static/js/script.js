822aaa6471d1fecbe1926cc9ca281c799b6548a4
Shobhit <imshobhit.sb@gmail.com>
2021-06-14 20:37:27 +0530

    Added styling

--- main/static/js/script.js
+++ main/static/js/script.js
@@ -1,1 +1,5 @@
-console.log("Hello from js file")
+window.addEventListener('scroll',function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 0)
+})
+// console.log("Hello")
