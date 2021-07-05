181ae5cc86345a49a6edfa8bdaa4f3a4e3939cbc
Shobhit <imshobhit.sb@gmail.com>
2021-07-05 13:57:19 +0530

    Added dynamic Skill Section

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Skill(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    percent = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+# class Project(models.Model):
+#     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+#     title = models.CharField(max_length=30)
+#     stack = models.CharField(max_length=50)
+#     description = models.TextField()
+#     image = models.ImageField(upload_to='uploads/products/')
+
+#     def __str__(self):
+#         return self.title
+
--- main/views.py
+++ main/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from .models import Category,Skill
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    category = Category.objects.all()
+    skill={}
+    # print(category)
+    for c in category:
+        skills = Skill.objects.filter(category=c)
+        
+        # print(skills)
+        a=list(skills)
+        # print(a)
+        # print(type(a))
+        # print(type(skills))
+        skill[c.name]=a
+    # print(category)
+    # print(context)
+    # for i in context:
+    #     # print(i)
+    #     for j in context[i]:
+    #         print(j.name)
+    return render(request,'index.html',{'skill':skill})
 
